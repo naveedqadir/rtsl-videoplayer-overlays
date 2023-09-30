4f84a48b1d2e57453cad9ecc101295814f5f15c6
naveedqadir <naveedqadir0@gmail.com>
2023-09-30 17:16:27 +0530

    done

--- client/src/routes/Routing.jsx
+++ client/src/routes/Routing.jsx
@@ -9,7 +9,6 @@ export default function Routing() {
           <Route path="/" element={<Home />} />
           <Route path="/app" element={<Video />} />
           <Route path="/overlaysettings" element={<OverlaySettings />} />
-          <Route path="/test" element={<Home />} />
         </Routes>
   );
 }
