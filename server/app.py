a160fbc00c3845e0bea92cbb7a1ad97183140440
Naveed Qadir <55054920+naveedqadir@users.noreply.github.com>
2023-09-30 17:05:06 +0530

    Update app.py
    Update

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://video-rtsl.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 mongo_uri = os.getenv("MONGO_URI")
 
 if mongo_uri:
